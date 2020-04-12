10e33c742449387ef30e23e81de57f5714c0e8f2
rahul.singh2 <rahul.singh2@dineout.co.in>
2020-04-12 09:30:17 +0530

    index page added

---
+++ basic.js
@@ -0,0 +1,10 @@
+
+    console.log('hi');
+
+    alert("working or not ?");
+    let name ="rahul";
+    console.log('hi ' + name);
+    for(name=1;name<=10;name++)
+    {
+        console.log("here it is " + name );
+    }
